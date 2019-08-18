Fix lti ode calculator, multiprocessig for scope, ode solver nonmonotonic calculation

pygears_control/lib/lti.py
@@ -1,4 +1,4 @@
-from pygears import gear
+from pygears import gear, alternative
 from scipy import signal
 from pygears.typing import Float
 import numpy as np
@@ -19,6 +19,18 @@ def lti(x: Float, *, num, den, init_x, init_y=None, clk_freq=None) -> Float:
     def lti_ode(t, y, x):
         res = ss.A @ np.vstack(y) + ss.B * x
         res.shape = (res.shape[0], )
-        return np.dot(ss.C, y), res
+        return float(np.dot(ss.C, y) + ss.D*x), res
 
     return ode(x, f=lti_ode, init_y=init_y, init_x=init_x, clk_freq=clk_freq)
+
+
+@alternative(lti)
+@gear
+def lti_ctrl(x: Float, *, sys, init_x, init_y=None, clk_freq=None) -> Float:
+
+    return lti(x,
+               num=sys.num[0][0],
+               den=sys.den[0][0],
+               init_x=init_x,
+               init_y=init_y,
+               clk_freq=clk_freq)

pygears_control/lib/ode.py
@@ -1,12 +1,26 @@
-from pygears import GearDone, gear
+from pygears import GearDone, gear, module
 from scipy.integrate import solve_ivp
 from pygears.typing import Float
 from .continuous import continuous
+from collections import deque
 
 
-def cont_wrap(f, qin, qout, init):
+def cont_wrap(f, qin, qout, init, m):
     def wrap(t, y):
-        dy = f(t, y, wrap.x)
+        x = wrap.x
+
+        # Sometimes ODE solver might go into the past trying to improve the
+        # accuracy. In that case we need to dig the appropriate set of inputs
+        # for that moment in time
+        if wrap.hist and t < wrap.hist[-1][1]:
+            for i in range(-2, -len(wrap.hist), -1):
+                if t >= wrap.hist[i][1]:
+                    x = wrap.hist[i+1][0]
+                    break
+            else:
+                x = init
+
+        dy = f(t, y, x)
 
         if isinstance(dy, tuple):
             outp = dy[0]
@@ -23,14 +37,15 @@ def cont_wrap(f, qin, qout, init):
         if qout.empty():
             qout.put(outp)
 
-        wrap.x = wrap.next_x
-        wrap.next_x, wrap.t = qin.get()
+        wrap.hist.append((wrap.x, wrap.t))
+        wrap.x, wrap.t = qin.get()
 
         if wrap.t is None:
             raise GearDone
 
         return dy
 
+    wrap.hist = deque([], 10)
     wrap.x = init
     wrap.next_x = init
     wrap.t = 0
@@ -43,7 +58,7 @@ def cont_wrap(f, qin, qout, init):
 def ode(x: Float, *, f, init_y, init_x, clk_freq=None) -> Float:
     def thread_function(qin, qout, clk_freq, init_x):
         try:
-            f_wrap = cont_wrap(f, qin, qout, init_x)
+            f_wrap = cont_wrap(f, qin, qout, init_x, module())
             solve_ivp(f_wrap, [0, float('inf')],
                       init_y,
                       max_step=1 / (2 * clk_freq),
@@ -51,4 +66,7 @@ def ode(x: Float, *, f, init_y, init_x, clk_freq=None) -> Float:
         except GearDone:
             pass
 
-    return continuous(x, f=thread_function, init_x=init_x, clk_freq=clk_freq)
+    return continuous(x,
+                      f=thread_function,
+                      init_x=float(init_x),
+                      clk_freq=clk_freq)